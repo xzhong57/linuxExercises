99c538afbfb37e0b8e786419290750d75acb2939
Will Wang <zwang3324@wisc.edu>
2024-10-17 14:13:54 -0500

    Will Wang school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | \
+    grep "MADISON SCHOOLS" | \
+    cut -d, -f7 | \
+    {
+	sum=0
+	count=0
+	while read -r value; do
+	    if [[ $value =~ ^[0-9]+$ ]]; then
+		# echo "value = $value ; count = $count; sum = $sum"
+		sum=$(($sum + $value))
+		count=$(($count + 1))
+	    else
+		echo "Warning: Non-numeric value encountered: $value"
+	    fi
+	done
+	average=$(echo "scale=2; $sum / $count" | bc)
+	echo "Average TotalAssessedValue: $average"
+    }
