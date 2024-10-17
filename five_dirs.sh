5d8580dcc9a45ca8a97e0bc3858fb244160cba22
Haorui Wu <hwu478@wisc.edu>
2024-10-17 14:02:54 -0500

    hwu eidt

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir five
-for i in {1..5};
-do  mkdir  five/dir$i
-    for j in {1..4};do
+for i in {1..5}; do
+    mkdir  five/dir$i
+    for j in {1..4}; do 
 	for k in $(seq $j); do
             echo $j >> five/dir$i/file$j;
 	done;
