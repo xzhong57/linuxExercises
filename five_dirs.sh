bf6cdbbfe8e76959f2992404d35c5f548f3e668e
Haorui Wu <hwu478@wisc.edu>
2024-10-17 13:49:42 -0500

    hwu478 edit the five_dirs.sh.

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+mkdir five
+for i in {1..5};
+do  mkdir  five/dir$i
+    for j in {1..4};do
+	for k in $(seq $j); do
+            echo $j >> five/dir$i/file$j;
+	done;
+    done;
+done
